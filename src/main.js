0b4c41ef0686023af619e358377e97e46c76c3d0
Anne Schumacher <anne.schumacher@gu.se>
2019-01-14 16:22:43 +0100

    get backend adress automatically

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 Vue.prototype.$backend =
-  // "https://api.kalufs.lol/recapi/";
-  "http://0.0.0.0:9005/";
+  process.env.NODE_ENV === "production"
+    ? "https://api.kalufs.lol/recapi/"
+    : "http://0.0.0.0:9005/";
 
 Vue.prototype.$defaultimg = Vue.prototype.$backend + "img/default.png";
 
