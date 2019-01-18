69c69f7368aebf1f50d4e76fa72acdd47b781504
Anne Schumacher <anne.schumacher@gu.se>
2019-01-18 16:53:06 +0100

    update packages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === "production" ? "recept/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "recept/" : "/",
   assetsDir: "./"
 };
