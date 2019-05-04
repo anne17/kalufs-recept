43587c416671035ec912a0937a031425cdba018f
Anne Schumacher <anne.schumacher@gu.se>
2019-05-04 22:06:06 +0200

    add authentication guards in router. Redirect not working yet...

--- src/router.js
+++ src/router.js
@@ -3,10 +3,11 @@ import Router from "vue-router";
 import Recipes from "./views/Recipes.vue";
 import EditRecipe from "./views/EditRecipe.vue";
 import ViewRecipe from "./views/ViewRecipe.vue";
+import { axios } from "./services.js";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -18,17 +19,23 @@ export default new Router({
     {
       path: "/suggestions",
       name: "suggestions",
-      component: Recipes
+      component: Recipes,
+      meta: {
+        requiresAuth: true,
+        allowGuest: true
+       }
     },
     {
       path: "/edit/:title",
       name: "edit",
-      component: EditRecipe
+      component: EditRecipe,
+      meta: { requiresAuth: true }
     },
     {
       path: "/suggest",
       name: "suggest",
-      component: EditRecipe
+      component: EditRecipe,
+      meta: { requiresAuth: true }
     },
     {
       path: "/view/:title",
@@ -37,3 +44,32 @@ export default new Router({
     }
   ]
 });
+
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // This route requires auth: check if logged in and redirect to login page.
+    axios
+      .post(Vue.prototype.$backend + "check_authentication")
+      .then(response => {
+        if (response.data.authenticated) {
+          if (!response.data.admin && !to.meta.allowGuest) {
+            // Don't go here
+            next({ path: from.path });
+          } else {
+            next();
+          }
+        } else {
+          next({
+            path: "#login",
+            query: { redirect: to.fullPath}
+          });
+        }
+      });
+
+  } else {
+    next();
+  }
+});
+
+export default router;
