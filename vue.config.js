d2eb399cb0842b4c7b9f79984ef7f9b7c56bcc6d
Anne Schumacher <anne.schumacher@gu.se>
2020-02-10 10:32:16 +0100

    fix publicPath bug

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack")
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "recept/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/recept/" : "/",
   productionSourceMap: false,
   configureWebpack: {
     plugins: [new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /sv/)]
